da0e97199c1b8542f40bcd0d07cdf183c212d99e
georgeStanescu <h_george_st@yahoo.com>
2014-07-09 18:58:47 +0300

    Added events on a contact item

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -7,15 +7,29 @@
 ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
   List.Contact = Marionette.ItemView.extend({
     tagName: "tr",
-    template: "#contact-list-item"
+    template: "#contact-list-item",
+    events:{
+    	'click':'highlightName'
+    },
+    highlightName: function(e){
+    	//e.preventDefault(); //preventing the default actionto happen
+    	this.$el.toggleClass('warning');
+    		//each view has an $el attribute returning a jQuery object wrapping the view’s DOM element: 
+    		//this.$el is equivalent to $(this.el)
+    		//in order to toggle the “warning” class on our tr containing the clicked item view
+    		//we simply call jQuery’s toggleClass method on the view’s jQuery object.
+    }
   });
 
   //CompositeView is used for scenarios where a collection needs to be rendered within a wrapper template
+  //In other words, CompositeView can be seen as a more powerful CollectionView with a template attribute
   List.Contacts = Marionette.CompositeView.extend({
     tagName: "table",
     className: "table table-hover", //added some styling class, so that Bootstrap will style it for us
     template: "#contact-list",	//we’ve specified the template our CompositeView should use
     itemView: List.Contact,
-    itemViewContainer: "tbody" //we’ve told the CompositeView to render the child views within the tbody element
+    itemViewContainer: "tbody" 	//we’ve told the CompositeView to render the child views within the tbody element
+    							//For more complex scenarios where you need more control on where/how to insert the rendered child views, 
+    							//you can override the appendHtml function in your CompositeView.
   });
 });
