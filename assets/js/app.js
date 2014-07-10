5e3a956f64765708c6f2e1c041e91ea404a70a97
georgeStanescu <h_george_st@yahoo.com>
2014-07-10 14:20:46 +0300

    Implemented a route step by step

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,8 @@ ContactManager.addRegions({
   mainRegion: "#main-region"
 });
 
+//navigate doesn’t just change the URL fragment, it also adds the new URL to the browser’s history. 
+//This, in turn, makes the browser’s “back” and “forward” buttons behave as expected
 ContactManager.navigate = function(route, options){
   options || (options = {});
   Backbone.history.navigate(route, options);
@@ -20,9 +22,20 @@ ContactManager.on("initialize:after", function(){
     Backbone.history.start();
     	//When using pushState, URL fragments look like the usual “/contacts/3” instead of “#contacts/3”
     	//to use pushState in your application your server has to respond to that URL
-      
+    
+    //if the user comes to our app at the root URL, let’s redirect him to “#contacts”
+    //this is done by using the ContactManager.getCurrentRoute()
     if(this.getCurrentRoute() === ""){
-      ContactManager.trigger("contacts:list");
+
+    	//Backbone.history.navigate("contacts"); //updating the URL with the proper fragment
+    	//ContactManager.ContactsApp.List.Controller.listContacts(); //executing the proper controller action, displaying the right view
+    		//the first two lines can be replaced by: Backbone.history.navigate("contacts", {trigger: true});
+    		//	or by the next two lines:
+
+    	this.navigate("contacts"); //equivalent to ContactManager.navigate("contacts");
+    	ContactManager.ContactsApp.List.Controller.listContacts();
+
+      	//ContactManager.trigger("contacts:list");
     }
   }
 });
