f846ddd091fb92d8a2796bc143ec478750a190ec
georgeStanescu <h_george_st@yahoo.com>
2014-07-10 22:48:21 +0300

    Added a loading view before displaying a contact

--- assets/js/apps/contacts/show/show_controller.js
+++ assets/js/apps/contacts/show/show_controller.js
@@ -2,6 +2,10 @@ ContactManager.module("ContactsApp.Show", function(Show, ContactManager,Backbone
   Show.Controller = {
   	//the controller receives an id as the argument 
 	showContact: function(id){
+		//create a new loading view and display it.
+		var loadingView = new ContactManager.Common.Views.Loading();
+	    ContactManager.mainRegion.show(loadingView);
+
 	  var fetchingContact = ContactManager.request("contact:entity", id); //we get the promise returned by our handler
 	  $.when(fetchingContact).done(function(contact){	
 	  	// then wait until the data has been fetched ($when...done) before displaying our view with the data
